c5067c7c1e0a166a11037d6f879f876ee689cefe
IshiiV <ishii.v.4@gmail.com>
2021-09-17 10:40:53 -0300

    revising putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: vishii <vishii@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 13:27:32 by vishii            #+#    #+#             */
-/*   Updated: 2021/09/16 11:52:05 by vishii           ###   ########.fr       */
+/*   Updated: 2021/09/17 10:39:41 by vishii           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*aux;
-
-	aux = ft_itoa(n);
-	ft_putstr_fd(aux, fd);
-	free(aux);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = n * -1;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + 48, fd);
 }
